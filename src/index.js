a240b05a341d5425559352ea8b0e296ac398c850
Jae Bradley <jae.b.bradley@gmail.com>
2020-12-27 20:03:56 -0500

    feat(autocomplete-extra): add autocompleteExtra method (#61)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
-const search = (term) => axios.get('https://api.urbandictionary.com/v0/define', { params: { term } }).then((response) => response.data);
+const API_BASE_PATH = 'https://api.urbandictionary.com';
 
-export { search }; // eslint-disable-line
+const autocompleteExtra = (term) => axios.get(`${API_BASE_PATH}/v0/autocomplete-extra`, { params: { term } }).then((response) => response.data);
+const search = (term) => axios.get(`${API_BASE_PATH}/v0/define`, { params: { term } }).then((response) => response.data);
+
+export {
+  autocompleteExtra,
+  search,
+};
